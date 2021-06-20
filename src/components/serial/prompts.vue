9048389dc575bbfc04331b01bc8d1e648808ce1f
0dr4d3k <odradek@wanadoo.es>
2021-06-20 17:01:36 +0200

    [server]  Default server changed [prompts]  Delete noSerialDialog popup

--- src/components/serial/prompts.vue
+++ src/components/serial/prompts.vue
@@ -27,60 +27,6 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <v-dialog
-      v-model="noSerialDialog"
-      persistent
-      max-width="500px"
-    >
-      <v-card>
-        <v-card-title class="text-h5">
-          Oh No! Your Browser isn't Supported!
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Unfortunately, your browser does not support the features we need to upload
-            code to your device. <!-- Currently supported browsers include Google Chrome, Opera,
-            and the new Microsoft Edge browser. --> Currently only Google Chrome is supported.
-          </p>
-          <p>
-            We are always working to try to make this application available to everyone,
-            but unfortunately, this may take some time, so we ask you to please use a supported
-            browser in the meantime.
-          </p>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <!-- <v-btn text href="https://caniuse.com/#feat=webusb" target="_blank">Which Browser Can I Use?</v-btn> -->
-          <v-btn href="https://www.google.com/chrome/" color="primary" @click.ctrl.shift.prevent="noSerialDialog = false">Get Chrome</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog
-      v-model="accessDeniedDialog"
-      max-width="500px"
-    >
-      <v-card>
-        <v-card-title class="text-h5">
-          Oh No! We cannot access the device!
-        </v-card-title>
-        <v-card-text>
-          <p>
-            For one reason or another, your operating system is blocking access
-            to the device. The reason for this can vary depending on your operating
-            system, below is a list of known fixes:
-          </p>
-          <ul>
-            <li><a href="https://playground.arduino.cc/Linux/All/#Permission" target="_blank">
-              Linux Serial Permissions
-            </a></li>
-          </ul>
-          <br>
-          <p>
-            If none of those apply/worked for you, please get in touch on our GitHub.
-          </p>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
   </div>
 </template>
 
